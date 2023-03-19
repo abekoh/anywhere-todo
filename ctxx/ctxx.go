9c3e0854a9d1429dc6ff27dd806234d9722e4711
Kotaro Abe <abekoh.bcky@gmail.com>
2023-03-19 13:03:13 +0900

    feat: set db

--- ctxx/ctxx.go
+++ ctxx/ctxx.go
@@ -1,1 +1,21 @@
 package ctxx
+
+import (
+	"context"
+	"database/sql"
+)
+
+type ctxKey int
+
+const (
+	dbKey ctxKey = iota
+)
+
+func SetDB(ctx context.Context, db *sql.DB) context.Context {
+	return context.WithValue(ctx, dbKey, db)
+}
+
+func GetDB(ctx context.Context) *sql.DB {
+	db, _ := ctx.Value(dbKey).(*sql.DB)
+	return db
+}

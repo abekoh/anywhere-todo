c2cc2c7bc9d1399d7e765a5b19e2693a7bb8ede9
Kotaro Abe <abekoh.bcky@gmail.com>
2023-03-21 10:31:43 +0900

    update

--- web/src/components/Card.tsx
+++ web/src/components/Card.tsx
@@ -14,21 +14,34 @@ type Props = {
   saveTask: (draftedTask: DraftedTask) => void;
 };
 
-export const TaskCard: React.FC<Props> = ({ task }) => (
-  <Card size="sm" direction={{ base: "column", sm: "row" }} p={2}>
-    <Checkbox size="lg" p={2} isChecked={task.done} />
-    <Stack>
-      <CardHeader>
-        <Input
-          size="lg"
-          fontWeight="bold"
-          variant="unstyled"
-          value={task.detail}
-        ></Input>
-      </CardHeader>
-      <CardBody>
-        <Input variant="unstyled" value={task.detail}></Input>
-      </CardBody>
-    </Stack>
-  </Card>
-);
+export const TaskCard: React.FC<Props> = ({ task, saveTask }) => {
+  const handleChange =
+    (key: keyof DraftedTask) =>
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
+      saveTask({ ...task, [key]: value });
+    };
+  return (
+    <Card size="sm" direction={{ base: "column", sm: "row" }} p={2}>
+      <Checkbox size="lg" p={2} isChecked={task.done} />
+      <Stack>
+        <CardHeader>
+          <Input
+            size="lg"
+            fontWeight="bold"
+            variant="unstyled"
+            value={task.detail}
+            onChange={handleChange("title")}
+          ></Input>
+        </CardHeader>
+        <CardBody>
+          <Input
+            variant="unstyled"
+            value={task.detail}
+            onChange={handleChange("detail")}
+          ></Input>
+        </CardBody>
+      </Stack>
+    </Card>
+  );
+};
